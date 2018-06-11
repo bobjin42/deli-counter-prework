65db2f8d5f0d78913fafda1a65dc1eb24ded8ec4
Yicheng Jin <bobjin42@gmail.com>
2018-06-11 18:07:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ end
 
 def now_serving(arr)
     if arr.length == 0
-    return "There is nobody waiting to be served!";
+    puts "There is nobody waiting to be served!";
   else
     served_name = arr.shift;
   end
